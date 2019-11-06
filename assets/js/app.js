0070d483baa7a335a2c54524a6b466d04ca92bd3
Seb974 <44438417+Seb974@users.noreply.github.com>
2019-11-06 15:08:12 +0400

    Initial files upload : First part

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import { Provider } from 'react-redux';
+import Navbar from './components/navbar';
+import ProductList from './components/productList';
+import ProductDetails from './components/productDetails';
+import Login from './components/login';
+import store from './store';
+import { loadUser } from './actions/authActions';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+require('../css/app.css');
+
+class App extends React.Component 
+{
+
+    state = {
+        cart: this.props.cart || [],
+    };
+
+    static propTypes = {
+        isAuthenticated: PropTypes.bool,
+        user: PropTypes.object
+    };
+
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                <span>
+                    <span id="react-header">
+                        <Navbar/>
+                    </span>
+                    <div id="page-container">
+                        {alert.message &&
+                            <div className={'alert ' + alert.type}>{alert.message}</div>
+                        }
+                            <Switch>
+                                <Route path='/' exact component={ProductList} />
+                                <Route path='/show/:id' component={ProductDetails} />
+                                <Route path='/login' component={Login} />       
+                                <Route path="/*" render={() => (<Redirect to="/" />)} /> 
+                            </Switch>
+                    </div>
+                </span>
+                </Router>
+            </Provider>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    user: state.auth.user,
+  });
+  
+  export default connect( mapStateToProps)(App);
+
+  ReactDOM.render(<App/>, document.getElementById("root"));
