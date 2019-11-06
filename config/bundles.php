5c48150dafc8de50e9407c301c468fa1642c18fc
Sébastien Maillot <sebastienmaillot@MacBook-Pro-de-Sebastien.local>
2019-11-06 18:21:37 +0400

    hot fix

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,7 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
