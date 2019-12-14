71ab4f8b981375700b1b5dc0309e9d90dd85d3a2
Seb97_4 <sebastien.maillot@coding-academy.fr>
2019-12-14 13:13:19 +0100

    CRUD supplier available

--- assets/js/components/admin/product/productForm.js
+++ assets/js/components/admin/product/productForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import { addUser, updateUser, deleteUser } from '../../../actions/userActions';
+import { addSupplier, updateSupplier, deleteSupplier } from '../../../actions/supplierActions';
 import { Link } from 'react-router-dom';
 import { Redirect } from "react-router-dom";
 import PropTypes from 'prop-types';
@@ -14,29 +14,12 @@ class ProductForm extends React.Component
         selection: typeof this.id === 'undefined' ? {} : this.selectedUser, 
         title: typeof this.id === 'undefined' ? 'Créer une nouvel utilisateur' : 'Modifier l\'utilisateur ' + this.selectedUser.username,
         user: (typeof this.props.token === 'undefined') ? {} : userExtractor(this.props.token),
-        username: typeof this.id === 'undefined' ? '' : this.selectedUser.username,
-        password: typeof this.id === 'undefined' ? '' : (typeof this.selectedUser.password === 'undefined' ? '' : this.selectedUser.password),
-        email: typeof this.id === 'undefined' ? '' : this.selectedUser.email,
-        isBanned: typeof this.id === 'undefined' ? false : this.selectedUser.isBanned,
-        roles: typeof this.id === 'undefined' ? ['ROLE_USER'] : this.selectedUser.roles,
-        phone: typeof this.id === 'undefined' ? '' : (typeof this.phone !== 'object' ? this.phone : this.phone.field),
-        d_address: typeof this.id === 'undefined' ? '' : (typeof this.d_address !== 'object' ? this.d_address : this.d_address.field),
-        d_address2: typeof this.id === 'undefined' ? '' : (typeof this.d_address2 !== 'object' ? this.d_address2 : this.d_address2.field),
-        d_zipCode: typeof this.id === 'undefined' ? '' : (typeof this.d_zipCode !== 'object' ? this.d_zipCode : this.d_zipCode.field),
-        b_address:  typeof this.id === 'undefined' ? '' : (typeof this.b_address !== 'object' ? this.b_address : this.b_address.field),
-        b_address2: typeof this.id === 'undefined' ? '' : (typeof this.b_address2 !== 'object' ? this.b_address2 : this.b_address2.field),
-        b_zipCode: typeof this.id === 'undefined' ? '' : (typeof this.b_zipCode !== 'object' ? this.b_zipCode : this.b_zipCode.field),
-        d_gps: typeof this.id === 'undefined' ? '-21.329519,55.471617' : (typeof this.d_gps !== 'object' ? this.d_gps : this.d_gps.field),
-        identicalBillingAddress: true,
-        d_city: '',
-        b_city: '',
-        cities: typeof this.props.cities === 'undefined' ? [] : this.props.cities
     };
 
     static propTypes = {
-        addUser: PropTypes.func.isRequired,
-        updateUser: PropTypes.func.isRequired,
-        deleteUser: PropTypes.func.isRequired
+        addSupplier: PropTypes.func.isRequired,
+        updateSupplier: PropTypes.func.isRequired,
+        deleteSupplier: PropTypes.func.isRequired
     };
     
     componentDidMount() {
@@ -65,129 +48,135 @@ class ProductForm extends React.Component
     }
 
     render = () => {
-        <span>
-            <h1>Create new Product</h1>
-            <form name="product" method="post" enctype="multipart/form-data">
-                <div id="product">
+        if( Object.entries(this.state.user).length !== 0 && this.state.user.roles.find(role => role === "ROLE_ADMIN") !== undefined ) {
+            return (
+                <span>
+                    <h1>Create new Product</h1>
+                    <form name="product" method="post" enctype="multipart/form-data">
+                        <div id="product">
 
-                    <div>
-                        <label for="product_supplier" class="required">Supplier</label>
-                        <select id="product_supplier" name="product[supplier]">
-                            <option value="1">Osaka</option>
-                            <option value="2">La Maison du Whisky</option>
-                            <option value="3">BurgerMary</option>
-                        </select>
-                    </div>
+                            <div>
+                                <label for="product_supplier" class="required">Supplier</label>
+                                <select id="product_supplier" name="product[supplier]">
+                                    <option value="1">Osaka</option>
+                                    <option value="2">La Maison du Whisky</option>
+                                    <option value="3">BurgerMary</option>
+                                </select>
+                            </div>
 
-                    <div>
-                        <label for="product_category" class="required">Category</label>
-                        <select id="product_category" name="product[category]">
-                            <option value="1">burger</option>
-                            <option value="2">boisson</option>
-                            <option value="3">produits laitiers</option>
-                            <option value="4">légumes</option>
-                            <option value="5">fruits</option>
-                            <option value="6">plats cuisinés</option>
-                        </select>
-                    </div>
+                            <div>
+                                <label for="product_category" class="required">Category</label>
+                                <select id="product_category" name="product[category]">
+                                    <option value="1">burger</option>
+                                    <option value="2">boisson</option>
+                                    <option value="3">produits laitiers</option>
+                                    <option value="4">légumes</option>
+                                    <option value="5">fruits</option>
+                                    <option value="6">plats cuisinés</option>
+                                </select>
+                            </div>
 
-                    <div>
-                        <label for="product_name" class="required">Name</label>
-                        <input type="text" id="product_name" name="product[name]" required="required" maxlength="150" />
-                    </div>
+                            <div>
+                                <label for="product_name" class="required">Name</label>
+                                <input type="text" id="product_name" name="product[name]" required="required" maxlength="150" />
+                            </div>
 
-                    <div>
-                        <label for="product_description">Description</label>
-                        <textarea id="product_description" name="product[description]"></textarea>
-                    </div>
+                            <div>
+                                <label for="product_description">Description</label>
+                                <textarea id="product_description" name="product[description]"></textarea>
+                            </div>
 
-                    <div>
-                        <label for="product_allergens">Allergens</label>
-                        <select id="product_allergens" name="product[allergens][]" multiple="multiple">
-                            <option value="1">anhydride sulfureux et sulfites</option>
-                            <option value="2">arachides</option>
-                            <option value="3">crustacés</option>
-                            <option value="4">gluten</option>
-                            <option value="5">oeufs</option>
-                            <option value="6">poissons</option>
-                            <option value="7">soja</option>
-                            <option value="8">lait</option>
-                            <option value="9">fruits à coques</option>
-                            <option value="10">céleri</option>
-                            <option value="11">moutarde</option>
-                            <option value="12">graines de sésame</option>
-                            <option value="13">lupin</option>
-                            <option value="14">mollusques</option>
-                        </select>
-                    </div>
+                            <div>
+                                <label for="product_allergens">Allergens</label>
+                                <select id="product_allergens" name="product[allergens][]" multiple="multiple">
+                                    <option value="1">anhydride sulfureux et sulfites</option>
+                                    <option value="2">arachides</option>
+                                    <option value="3">crustacés</option>
+                                    <option value="4">gluten</option>
+                                    <option value="5">oeufs</option>
+                                    <option value="6">poissons</option>
+                                    <option value="7">soja</option>
+                                    <option value="8">lait</option>
+                                    <option value="9">fruits à coques</option>
+                                    <option value="10">céleri</option>
+                                    <option value="11">moutarde</option>
+                                    <option value="12">graines de sésame</option>
+                                    <option value="13">lupin</option>
+                                    <option value="14">mollusques</option>
+                                </select>
+                            </div>
 
-                    <div>
-                        <label for="product_proteins">Proteins</label>
-                        <input type="text" id="product_proteins" name="product[proteins]"/>
-                    </div>
+                            <div>
+                                <label for="product_proteins">Proteins</label>
+                                <input type="text" id="product_proteins" name="product[proteins]"/>
+                            </div>
 
-                    <div>
-                        <label for="product_carbohydrates">Carbohydrates</label>
-                        <input type="text" id="product_carbohydrates" name="product[carbohydrates]" />
-                    </div>
+                            <div>
+                                <label for="product_carbohydrates">Carbohydrates</label>
+                                <input type="text" id="product_carbohydrates" name="product[carbohydrates]" />
+                            </div>
 
-                    <div>
-                        <label for="product_sugar">Sugar</label>
-                        <input type="text" id="product_sugar" name="product[sugar]"/>
-                    </div>
+                            <div>
+                                <label for="product_sugar">Sugar</label>
+                                <input type="text" id="product_sugar" name="product[sugar]"/>
+                            </div>
 
-                    <div>
-                        <label for="product_fat">Fat</label>
-                        <input type="text" id="product_fat" name="product[fat]"/>
-                    </div>
+                            <div>
+                                <label for="product_fat">Fat</label>
+                                <input type="text" id="product_fat" name="product[fat]"/>
+                            </div>
 
-                    <div>
-                        <label for="product_saturated">Saturated</label>
-                        <input type="text" id="product_saturated" name="product[saturated]"/>
-                    </div>
+                            <div>
+                                <label for="product_saturated">Saturated</label>
+                                <input type="text" id="product_saturated" name="product[saturated]"/>
+                            </div>
 
-                    <div>
-                        <label for="product_sodium">Sodium</label>
-                        <input type="text" id="product_sodium" name="product[sodium]"/>
-                    </div>
+                            <div>
+                                <label for="product_sodium">Sodium</label>
+                                <input type="text" id="product_sodium" name="product[sodium]"/>
+                            </div>
 
-                    <div>
-                        <label for="product_tva" class="required">Tva</label>
-                        <select id="product_tva" name="product[tva]">
-                            <option value="1">2.1</option>
-                            <option value="2">8.5</option>
-                        </select>
-                    </div>
+                            <div>
+                                <label for="product_tva" class="required">Tva</label>
+                                <select id="product_tva" name="product[tva]">
+                                    <option value="1">2.1</option>
+                                    <option value="2">8.5</option>
+                                </select>
+                            </div>
 
-                    <div>
-                        <label for="product_picture">Illustration</label>
-                        <input type="file" id="product_picture" name="product[picture]"/>
-                    </div>
+                            <div>
+                                <label for="product_picture">Illustration</label>
+                                <input type="file" id="product_picture" name="product[picture]"/>
+                            </div>
 
-                    <div>
-                        <label class="required">Variants</label>
-                        <div id="product_variants" data-prototype="<div ><label class=&quot;required&quot;>__name__label__</label><div id=&quot;product_variants___name__&quot;><div ><label for=&quot;product_variants___name___name&quot; class=&quot;required&quot;>Name</label><input type=&quot;text&quot; id=&quot;product_variants___name___name&quot; name=&quot;product[variants][__name__][name]&quot; required=&quot;required&quot; maxlength=&quot;60&quot; /></div><div ><label for=&quot;product_variants___name___price&quot; class=&quot;required&quot;>Price</label><input type=&quot;text&quot; id=&quot;product_variants___name___price&quot; name=&quot;product[variants][__name__][price]&quot; required=&quot;required&quot;/></div></div></div>"></div>
-                        <div>
-                            <label class="required">Variant N°1</label>
-                            <div id="product_variants_0">
+                            <div>
+                                <label class="required">Variants</label>
+                                <div id="product_variants" data-prototype="<div ><label class=&quot;required&quot;>__name__label__</label><div id=&quot;product_variants___name__&quot;><div ><label for=&quot;product_variants___name___name&quot; class=&quot;required&quot;>Name</label><input type=&quot;text&quot; id=&quot;product_variants___name___name&quot; name=&quot;product[variants][__name__][name]&quot; required=&quot;required&quot; maxlength=&quot;60&quot; /></div><div ><label for=&quot;product_variants___name___price&quot; class=&quot;required&quot;>Price</label><input type=&quot;text&quot; id=&quot;product_variants___name___price&quot; name=&quot;product[variants][__name__][price]&quot; required=&quot;required&quot;/></div></div></div>"></div>
                                 <div>
-                                    <label for="product_variants_0_name" class="required">Name</label>
-                                    <input type="text" id="product_variants_0_name" name="product[variants][0][name]" required="required" maxlength="60"/>
+                                    <label class="required">Variant N°1</label>
+                                    <div id="product_variants_0">
+                                        <div>
+                                            <label for="product_variants_0_name" class="required">Name</label>
+                                            <input type="text" id="product_variants_0_name" name="product[variants][0][name]" required="required" maxlength="60"/>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+                            <div>
+                                <label for="product_variants_0_price" class="required">Price</label>
+                                <input type="text" id="product_variants_0_price" name="product[variants][0][price]" required="required"/>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <label for="product_variants_0_price" class="required">Price</label>
-                        <input type="text" id="product_variants_0_price" name="product[variants][0][price]" required="required"/>
-                    </div>
-                </div>
-                <a href="#" class="btn btn-danger">Supprimer</a>
-                <input type="hidden" id="product__token" name="product[_token]" value="8Bc8PRm8WyrO7AH69sVEuaIWv8u3VYCTro8tvEPwP9I" />
-                <a href="#" id="add_variant" class="btn btn-default">Add a variant</a>
-                <button class="btn">Save</button>
-            </form>
-        </span>
+                        <a href="#" class="btn btn-danger">Supprimer</a>
+                        <input type="hidden" id="product__token" name="product[_token]" value="8Bc8PRm8WyrO7AH69sVEuaIWv8u3VYCTro8tvEPwP9I" />
+                        <a href="#" id="add_variant" class="btn btn-default">Add a variant</a>
+                        <button class="btn">Save</button>
+                    </form>
+                </span>
+            )
+        } else {
+            return <Redirect to='/'/>
+        }
     }
 
 }
@@ -200,4 +189,4 @@ const mapStateToProps = state => ({
     user: state.auth.user,
 });
 
-export default connect(mapStateToProps, { addUser, updateUser, deleteUser })(ProductForm);
+export default connect(mapStateToProps, { addSupplier, updateSupplier, deleteSupplier })(ProductForm);
